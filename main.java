8255a9ba0228bd01ed2de8d418b67e53faaf0161
ketaki07 <udipi.k@husky.neu.edu>
2020-03-20 00:26:04 -0400

    changes in code to take arguments from command line

--- main.java
+++ main.java
@@ -1,10 +1,12 @@
 import java.util.HashMap;
-
+//To determine whether a one-to-one character mapping exists from one string, s1, to another string,
+//s2.
 public class main {
 
   public static boolean charMapping(String string1, String string2){
     int strLength1= string1.length();
     int strLength2 = string2.length();
+    //If length of two strings is different return false
     if(strLength1 != strLength2){
       return false;
     }
@@ -13,6 +15,10 @@ public class main {
     for(int i=0;i<strLength1;i++){
       char char1=string1.charAt(i);
       char char2=string2.charAt(i);
+      //if character has already occured in string1 then check in hashmap if its corresponding
+      //value is same as the current character in string2. If not return false.
+      //if character has not already occured in string1 then just add that character and its
+      //corresponding character from string2 in hashmap.
       if(hashStr1.containsKey(char1)) {
         if (char2 != hashStr1.get(char1)) {
           return false;
@@ -25,7 +31,7 @@ public class main {
     }
 
   public static void main(String[] args) {
-    boolean value = charMapping("abc","bcd");
+    boolean value = charMapping(args[0],args[1]);
     System.out.println(value);
   }
   }
